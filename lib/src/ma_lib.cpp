f5c1152379ee08e3d2948bb55931a4bc36aa4d26
Cyril-Grelier <cyril.grelier@outlook.fr>
2025-07-17 17:00:16 +0200

    remove intentional bug

--- lib/src/ma_lib.cpp
+++ lib/src/ma_lib.cpp
@@ -1,12 +1,5 @@
 #include "ma_lib.h"
 
 int add(int a, int b) {
-
-    int *ptr = new int[10];
-    // Buffer overflow: index 10 is out of bounds (0-9)
-    // Sanitizer will detect it at runtime in debug mode
-    ptr[10] = 42;
-    // Memory leak: ptr is not freed
-
     return a + b;
 }
